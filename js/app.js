0ded4bd1eedd278a98e3c30dc09b1c2298ba1673
Riyad Khan <riyad.uiu@gmail.com>
2018-04-11 02:08:56 +0600

    Fix localstorage saving problem on browser reload

--- js/app.js
+++ js/app.js
@@ -1,72 +1,65 @@
-// default bookmark list
-var bookmarks = [
-    {
-        title: 'Google',
-        url: 'https://google.com/'
-    },
-    {
-        title: 'Facebook',
-        url: 'https://facebook.com/'
-    },
-    {
-        title: 'Yahoo',
-        url: 'https://yahoo.com/'
-    },
-    {
-        title: 'Linked In',
-        url: 'https://linkedin.com/'
-    },
-    {
-        title: 'Github',
-        url: 'https://github.com/'
-    },
-    {
-        title: 'Twitter',
-        url: 'https://twitter.com/'
-    },
+document.getElementById('myForm').addEventListener('submit', BookmarkSave);
 
-]
-// to clear input field
+//to clear input field
 function clear() {
     document.getElementById('title').value = '';
     document.getElementById('url').value = '';
 }
-// to save new bookmark
-function BookmarkSave() {
+//to save new bookmark
+function BookmarkSave(e) {
     var title = document.getElementById('title').value;
     var url = document.getElementById('url').value;
     if(title != '' && url != '') {
         var obj = {
             title: title,
             url: url,
+        };
+    }
+        if(localStorage.getItem('bookmarks') === null) {
+            // default bookmark list
+            var bookmarks = [];
+            bookmarks.push(obj);
+
+            localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        } else {
+            var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+            bookmarks.push(obj);
+            localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
         }
-        bookmarks.push(obj);
+        console.log(title);
+        
         InitialBookmark();
         clear();
-    } 
+    e.preventDefault();
 }
-// to show all bookmark list
-InitialBookmark() 
+//to show all bookmark list
+InitialBookmark(); 
 function InitialBookmark() {
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-    var output = JSON.parse(localStorage.getItem('bookmarks'));
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
     var colorArray = ['success', 'info', 'warning', 'danger'];
-    var colorIndex = Math.floor(Math.random()*colorArray.length);
     var innerValue = '';
-    for (let i = 0; i < bookmarks.length; i++) {
-        console.log(bookmarks[i].title);
-        innerValue += '<div class="col-md-6"><div class="alert alert-' + colorArray[colorIndex] + '"><strong><a href="' + bookmarks[i].url + '" target="_blank">' + bookmarks[i].title + '</a></strong><button type="button" class="close" onclick="DeleteBookmark('+"'"+ bookmarks[i].url+ "'"+')">&times;</button></div></div>'
+    for (var i = 0; i < bookmarks.length; i++) {
+        var colorIndex = Math.floor(Math.random()*colorArray.length);
+        innerValue += '<div class="col-md-6"><div class="alert alert-' + colorArray[colorIndex] + '"><strong><a href="' + bookmarks[i].url + '" target="_blank">' + bookmarks[i].title + '</a></strong><button type="button" class="close" onclick="DeleteBookmark('+"'"+ bookmarks[i].url+ "'"+')">&times;</button></div></div>';
     }
     document.getElementById('savedBookmark').innerHTML = innerValue;
+    if(bookmarks != '') {
+        document.getElementById('empty_data').innerHTML = '';
+    } else {
+        document.getElementById('empty_data').innerHTML = "{ you haven't added bookmark }";
+    }
     
 }
 //to delete bookmark
 function DeleteBookmark(url) {
-    for (let i = 0; i < bookmarks.length; i++) {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    for (var i = 0; i < bookmarks.length; i++) {
         if (url === bookmarks[i].url) {
             bookmarks.splice(i, 1);
         }
         
     }
-    InitialBookmark() 
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    InitialBookmark();
 }
