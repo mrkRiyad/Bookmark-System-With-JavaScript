a5cbeff97785608d9d353cc6f5bd57dc3b165e60
Riyad Khan <riyad.uiu@gmail.com>
2018-04-11 03:32:11 +0600

    cleaning and commenting code

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
+// form submit event
 document.getElementById('myForm').addEventListener('submit', BookmarkSave);
+
+// to show all bookmark list
+InitialBookmark(); 
 
 //to clear input field
 function clear() {
     document.getElementById('title').value = '';
     document.getElementById('url').value = '';
 }
-//to save new bookmark
+// to save new bookmark
 function BookmarkSave(e) {
     var title = document.getElementById('title').value;
     var url = document.getElementById('url').value;
+
+    // empty field submit control condition
     if(title != '' && url != '') {
         var obj = {
             title: title,
             url: url,
         };
-    
+        
         if(localStorage.getItem('bookmarks') === null) {
             // default bookmark list
             var bookmarks = [];
@@ -23,26 +29,36 @@ function BookmarkSave(e) {
             localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
         } else {
             var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
             bookmarks.push(obj);
             localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 
         }
-    }  
-        InitialBookmark();
-        clear();
+    }   
+    // show bookmark data
+    InitialBookmark();
+
+    // clear input field after add bookmark
+    clear();
+
+    // remove event bubble
     e.preventDefault();
 }
-//to show all bookmark list
-InitialBookmark(); 
+
+// to show all bookmark data
 function InitialBookmark() {
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
     var colorArray = ['success', 'info', 'warning', 'danger'];
+    var colorIndex;
     var innerValue = '';
     for (var i = 0; i < bookmarks.length; i++) {
-        var colorIndex = Math.floor(Math.random()*colorArray.length);
+        colorIndex = Math.floor(Math.random()*colorArray.length);
         innerValue += '<div class="col-md-6"><div class="alert alert-' + colorArray[colorIndex] + '"><strong><a href="' + bookmarks[i].url + '" target="_blank">' + bookmarks[i].title + '</a></strong><button type="button" class="close" onclick="DeleteBookmark('+"'"+ bookmarks[i].url+ "'"+')">&times;</button></div></div>';
     }
+    // assign bookmark data to DOM
     document.getElementById('savedBookmark').innerHTML = innerValue;
+
+    // empty data message condition
     if(bookmarks != '') {
         document.getElementById('empty_data').innerHTML = '';
     } else {
@@ -50,7 +66,7 @@ function InitialBookmark() {
     }
     
 }
-//to delete bookmark
+// to delete bookmark
 function DeleteBookmark(url) {
     var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
     for (var i = 0; i < bookmarks.length; i++) {
@@ -60,5 +76,7 @@ function DeleteBookmark(url) {
         
     }
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+    // show bookmark data
     InitialBookmark();
 }
